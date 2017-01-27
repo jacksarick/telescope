bfe73ba3eee62f09234e82fd5597a91d0d14ecad
Jack Sarick <jacksarick@gmail.com>
2017-01-27 11:56:31 -0500

    Bound mouse

--- telescope.tcl
+++ telescope.tcl
@@ -5,7 +5,9 @@ puts [source "server.tcl"]
 pack [canvas .c] -fill both -expand 1
 
 # Pipe keyboard output to steam
-bind . <KeyPress> { send_data %A }
+bind . <KeyPress> { send_data "key %A" }
+bind . <ButtonPress> { send_data "click %b %x %y" }
+bind . <Motion> { send_data "mouse %x %y" }
 
 # Start Server
 set sock [socket -server connect 1234]

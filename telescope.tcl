426066771bc3388b1c328718276ef682e150c5e0
Jack Sarick <jacksarick@gmail.com>
2017-01-27 12:00:20 -0500

    Added a few things for dev

--- telescope.tcl
+++ telescope.tcl
@@ -1,12 +1,17 @@
 package require Tk
 puts [source "server.tcl"]
 
+# Bind close button to exit program
+wm protocol . WM_DELETE_WINDOW {
+	exit
+}
+
 # Make canvas
 pack [canvas .c] -fill both -expand 1
 
 # Pipe keyboard output to steam
 bind . <KeyPress> { send_data "key %A" }
-bind . <ButtonPress> { send_data "click %b %x %y" }
+# bind . <ButtonPress> { send_data "click %b %x %y" }
 bind . <Motion> { send_data "mouse %x %y" }
 
 # Start Server

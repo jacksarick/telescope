37d4d92aaf128fbac6ffd2779bc218828115b219
Jack Sarick <jacksarick@gmail.com>
2017-01-27 17:02:17 -0500

    Added more actions

--- telescope.tcl
+++ telescope.tcl
@@ -10,9 +10,11 @@ wm protocol . WM_DELETE_WINDOW {
 pack [canvas .c] -fill both -expand 1
 
 # Pipe keyboard output to steam
-bind . <KeyPress> { send_data "key %A" }
-bind . <ButtonPress> { send_data "click %b %x %y" }
-# bind . <Motion> { send_data "mouse %x %y" }
+bind . <KeyPress> { send_data "keypress %A" }
+bind . <KeyRelease> { send_data "keyrelese %A" }
+bind . <ButtonPress> { send_data "buttonpress %b %x %y" }
+bind . <ButtonRelease> { send_data "buttonrelease %b %x %y" }
+bind . <Motion> { send_data "mouse %x %y" }
 
 # Start Server
 set sock [socket -server connect 1234]

0a8c33018456b8be9f54e3b81009e2fc00886481
Jack Sarick <jacksarick@gmail.com>
2017-01-30 22:04:18 -0500

    Now uses images

--- canvas.tcl
+++ canvas.tcl
@@ -1,6 +1,6 @@
-proc name {args} {
-	
-}
-proc draw_pixel {c x y r g b} {
-	$c create rectangle $x $y [incr $x] [incr $y] -fill #$r$g$b -width 0
+puts $::img
+
+proc draw_pixel {x y hex} {
+	puts "$x $y [incr x] [incr y]"
+	$::img put #$hex -to $x $y [incr x] [incr y]
 }

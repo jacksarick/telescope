174ea1c14989e4aa580cb2556faeb5033f48f4bd
Jack Sarick <jacksarick@gmail.com>
2017-01-30 22:32:08 -0500

    everything appears to work, but I am skeptical

--- canvas.tcl
+++ canvas.tcl
@@ -1,4 +1,19 @@
+# Make canvas
+canvas .c -bg black
+pack .c -fill both -expand 1
+
+# Make image within canvas
+set ::img [image create photo -palette 256/256/256]
+.c create image 0 0 -image $::img -anchor nw
+
 proc draw_pixel {x y hex} {
 	puts [image width $::img]
 	$::img put #$hex -to $x $y
 }
+
+proc img_size {x y} {
+	# $::img configure -width $x -height $y
+	puts [$::img cget -width]
+}
+
+return "canvas loaded"
